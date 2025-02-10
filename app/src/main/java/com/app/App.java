ac67e9824dfe71081f28899b3cabdc9f4e78fc44
Angel Cruz <angcruz8333@gmail.com>
2025-02-10 17:39:56 -0600

    Adding changes in app

---
+++ app/src/main/java/com/app/App.java
@@ -0,0 +1,11 @@
+package com.app;
+
+import com.gui.GraphicInterface;
+
+public class App {
+    public static void main(String[] args)
+    {
+        GraphicInterface gui = new GraphicInterface();
+
+    }
+}
